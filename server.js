97e92bc42188d71d0ca633e40da5472bb154b855
Fernando Moreno <Fer.moreno0414@gmail.com>
2022-03-13 21:27:52 -0400

    finished module

--- server.js
+++ server.js
@@ -1,65 +1,18 @@
 const express = require('express');
-const { animals } = require('./data/animals');
+
 const PORT = process.env.PORT || 3001;
-
-
-// assigned express.js to the variable express
-// getting express to work
 const app = express();
+const apiRoutes = require('./routes/apiRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
-//this will filkter what we need
-function filterByQuery(query, animalsArray) {
-    let personalityTraitsArray = [];
-    // Note that we save the animalsArray as filteredResults here:
-    let filteredResults = animalsArray;
-    if (query.personalityTraits) {
-      // Save personalityTraits as a dedicated array.
-      // If personalityTraits is a string, place it into a new array and save.
-      if (typeof query.personalityTraits === 'string') {
-        personalityTraitsArray = [query.personalityTraits];
-      } else {
-        personalityTraitsArray = query.personalityTraits;
-      }
-      // Loop through each trait in the personalityTraits array:
-      personalityTraitsArray.forEach(trait => {
-        // Check the trait against each animal in the filteredResults array.
-        // Remember, it is initially a copy of the animalsArray,
-        // but here we're updating it for each trait in the .forEach() loop.
-        // For each trait being targeted by the filter, the filteredResults
-        // array will then contain only the entries that contain the trait,
-        // so at the end we'll have an array of animals that have every one 
-        // of the traits when the .forEach() loop is finished.
-        filteredResults = filteredResults.filter(
-          animal => animal.personalityTraits.indexOf(trait) !== -1
-        );
-      });
-    }
-    if (query.diet) {
-      filteredResults = filteredResults.filter(animal => animal.diet === query.diet);
-    }
-    if (query.species) {
-      filteredResults = filteredResults.filter(animal => animal.species === query.species);
-    }
-    if (query.name) {
-      filteredResults = filteredResults.filter(animal => animal.name === query.name);
-    }
-    // return the filtered results:
-    return filteredResults;
-}
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
 
-//this is opening a local host and showing "hello"
-app.get('/api/animals', (req, res) => {
-    let results = animals;
-    if (req.query){
-        results = filterByQuery(req.query, results);
-    }
-    
-    console.log(req.query)
-    res.json(results);
-});
-  
-// this is telling the app where to go 
+// Use apiRoutes
+app.use('/api', apiRoutes);
+app.use('/', htmlRoutes);
+
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
-  });
-
+  console.log(`API server now on port ${PORT}!`);
+});
